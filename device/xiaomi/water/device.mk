43373acba9389b53aceef367dd5302642a26855a
suprimeleo <greenleonardo06est@email.com>
2025-01-18 21:42:55 +0200

    Added device tree of Redmi A2

---
+++ device/xiaomi/water/device.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := device/xiaomi/ice
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Virtual A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+
+PRODUCT_DEVICE := water
+PRODUCT_NAME := twrp_water
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi A2
+PRODUCT_MANUFACTURER := Xiaomi
